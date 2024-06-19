394750e4c754c30db5c6a1b70afa2deed7fb0939
Allen John <88543723+Allen9994@users.noreply.github.com>
2024-06-20 02:18:31 +0530

    Simplified code

--- Tetris_new.cpp
+++ Tetris_new.cpp
@@ -6,7 +6,6 @@
 #endif
 #include <iostream>
 #include <fstream>
-#include <vector>
 #include <algorithm>
 #include <chrono>
 #include <thread>
@@ -47,7 +46,7 @@ void speedSelector();
 
 void destroy()
 {
-    pos=area-1;
+    pos = area-1;
     while(pos >= side)
     {
         if(map[pos] == 'x') block++;
@@ -55,7 +54,7 @@ void destroy()
         {
             score++;
             k = pos-1;
-            while(k>=0)
+            while(k >= 0)
             {
                 map[k+side] = map[k];
                 k--;
@@ -83,10 +82,7 @@ void convert()
 void randomize()
 {
     srand((unsigned) time(0));
-    for (short index = 0; index < area; index++) 
-    {
-        p[index] = (rand() % 6) + 1;
-    }
+    for (short index = 0; index < area; index++) p[index] = (rand() % 6) + 1;
 }
 void shape()
 {
@@ -121,8 +117,7 @@ void shape()
         map[head-side] = map[head+1] = map[head-side+1] = 'x';
         if (map[head+side+1] == 'x') head = 2;
     }
-    if (map[head+side] == 'x') head = 2;
-    if (head >= area-side && head <= area) head = 2;
+    if (map[head+side] == 'x' || (head >= area-side && head <= area)) head = 2;
 }
 void read_value() //inputting value from user
 {   
@@ -139,8 +134,7 @@ void read_value() //inputting value from user
         case 'a': value = c;
     }
     if (p[counter]== 6 && (value == 'a' || value == 'd') && (map[head+side] != 'x' || map[head+side+1] != 'x')) map[last+1] = ' ';
-tcsetattr( STDIN_FILENO, TCSANOW, &oldt);
-  
+    tcsetattr( STDIN_FILENO, TCSANOW, &oldt);
     cv.notify_one();
 }
 void take_input() //function to accept the value parallelly while game is proceeding.  
@@ -163,7 +157,7 @@ void process()   //Brain of the program. Entire game operation happens here.
 {
     system("clear");
     if (map[head+side] == 'x' && head <= side) gameToggle(score,false);
-    head+=side;
+    head += side;
     value = 'q';
     shape();
     if (head == 2) {
@@ -193,31 +187,30 @@ int main()
     randomize();
     mainMenu();
 }
-void gameToggle(short score, bool toggle) //To exit the game when the snake bites itself
+void gameToggle(short score, bool toggle) 
 {   
     do{
-        if(!toggle)
+        if(toggle) take_input();
+        else
         {
-            system("clear");
             fileManage(to_string(pace) + to_string(level),'s');
             fileManage(to_string(score),'o');
             cout<<"Game Over!\nScore:"<<score;
             exit(0);
         }
-        take_input();
-    }while(1);
+    }while(toggle);
 }
 void mainMenu()   //Main Menu
 {   
     gameToggle(0,true);
-    char choice='z';
+    char choice = 'z';
     cout<<"\nCreated by Allen\n";
     cout<<"Press:\n1 to Play\n2 for Help\n3 for Game Settings\n4 to exit\n";
     cin>>choice;
+    system("clear");
     if(choice == '1') gameToggle(0,true);
     if(choice == '2')   //Instructions
     {   
-        system("clear");
         cout<<"CONTROLS\nPRESS\n w TO MOVE UPWARD\n s TO MOVE DOWNWARDS \n d TO MOVE RIGHT \n a TO MOVE LEFT";
         cin>>choice;
         system("clear");
@@ -225,7 +218,6 @@ void mainMenu()   //Main Menu
     }
     if(choice == '3')
     {
-        system("clear");
         cout<<"Control the Block Speed. PRESS\n1 : Easy\n2 : Medium\n3 : Hard\n";
         cin>>pace;
         cout<<"Control the Game Difficulty level. PRESS\n1 : LEVEL 1\n2 : LEVEL 2\n";
@@ -249,7 +241,7 @@ void fileManage(string data, char option)
             while (fin.good()) {
                 getline(fin,s);
             }
-            pace =  s[0] - '0';
+            pace  = s[0] - '0';
             level = s[1] - '0';
             if (highscore < stoi(s.substr(2))) highscore = stoi(s.substr(2)) ;
             cout<<highscore;
